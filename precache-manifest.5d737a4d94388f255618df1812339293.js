f40ce797ffeb6c529ec4f774b32f5cdd1728ddb3
mospy26 <m.mustafaahmed@hotmail.com>
2020-03-17 03:25:48 +1100

    Updates

---
+++ precache-manifest.5d737a4d94388f255618df1812339293.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13fcc8e90b731c15d380bb7a57d32be1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aacfa75a936f4dc9e4af",
+    "url": "/static/js/2.7afc2554.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7afc2554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61540767509b733ff61a",
+    "url": "/static/js/main.03105f92.chunk.js"
+  },
+  {
+    "revision": "7eb589e7d1034a311e8a",
+    "url": "/static/js/runtime-main.ccff8f02.js"
+  }
+]);
